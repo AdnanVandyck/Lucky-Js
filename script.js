b1aa3f32d456f9ec7a1643223ef78e239c726279
Adnan <adnanvandyck@gmail.com>
2022-12-10 14:50:08 -0500

    added inital files

---
+++ script.js
@@ -0,0 +1,2 @@
+let suits = ["Bulls", "Tarheels", "Wizards", "Hornets"]
+let values = ["Jumpman", "Fourteen", "Thirteen", "Twelve", "Eleven", "Ten", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"]
