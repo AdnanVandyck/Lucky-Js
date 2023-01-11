2765d32890a2a91bdfabe9827d41495aacb37825
Adnan <adnanvandyck@gmail.com>
2023-01-11 12:54:30 -0500

    added css

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let suits = ["Bulls", "Tarheels", "Wizards", "Hornets"]
-let values = ["Jumpman", "Fourteen", "Thirteen", "Twelve", "Eleven", "Ten", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"]
+let suits = ["Bulls", "Tarheels", "Wizards", "Hornets"];
+let values = ["Jumpman", "AJ14", "AJ13", "AJ12", "AJ11", "AJ10", "AJ8", "AJ7", "AJ6", "AJ5", "AJ4", "AJ3", "AJ2", "AJ1"];
+
+function createDeck() {
+    let deck = new Array 
+
+}
